b0dd2608d9d896742b96619faa71181d2ce254fa
“Sergio <sergiogiuseppecofano@gmail.com>
2023-12-06 16:49:29 +0100

    carica progetto

---
+++ script.js
@@ -0,0 +1,14 @@
+// alert ('Come ti chiami?') ;
+
+// let name = prompt () ;
+
+// let presentazione = 'Ciao a tutti mi chiamo ' + name;
+
+// alert(presentazione)
+alert richiesta ('Vuoi una birra?')
+let richiesta = ('Vuoi una birra? \n si \n no')
+
+while (richiesta !=si && richiesta !=no) { richiesta= prompt ('Vuoi una birra \n si \n no')}
+
+let age = prompt ('Quanti anni hai?') ;
+
